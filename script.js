67ff92724ee1ef991ad59810e1350d7f85b37fcb
erotourtes <erotourtes@gmail.com>
2021-04-06 21:24:39 +0300

    without 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let a = document.querySelector(".ph")
 let b = a.getElementsByTagName("*")
 for(el of b)
-	el.style.width = window.innerWidth / 4 + "px"
+	el.style.width = window.innerWidth / 4 - 5 + "px"

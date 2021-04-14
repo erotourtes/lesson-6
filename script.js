13cced13062d9d7a7c726ab1d3bc5d35b155aafc
erotourtes <erotourtes@gmail.com>
2021-04-14 11:54:58 +0300

    ready

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+ScrollReveal().reveal("div")
+
+let header = document.querySelector("header")
+header.style.backgroundImage = "url(img/bg.jpg)"
+
+
 let a = document.querySelector(".ph")
 let b = a.getElementsByTagName("*")
 for(el of b)
 	el.style.width = window.innerWidth / 4 - 5 + "px"
+
+let all = document.querySelector("div")

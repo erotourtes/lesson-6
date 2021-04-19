19ecc959b33e79760ae7c3e7f1dc8e72093cac33
erotourtes <erotourtes@gmail.com>
2021-04-19 11:58:12 +0300

    ready

---
+++ carusel/carusel.js
@@ -0,0 +1,46 @@
+	const dots = $(".dots")
+	
+	const images = $(".photosCarusel").children()
+
+	images.hide()
+	$("#caruselPhoto0").show()
+	
+	let indexPhoto = 0
+	let prevImg = $("#caruselPhoto0")
+	for(let i = 0; i < images.length; i++) {
+		let el = $("<div></div>");
+		if (i == indexPhoto)
+			el.addClass("selectedDot")
+		el.click(()=>{
+			$(`.photosCarusel img:nth-child(${indexPhoto + 1})`).slideUp("fast")
+			let curImg = $(`#caruselPhoto${i}`)
+			indexPhoto = i
+			
+			prevImg.slideUp("fast")
+			curImg.slideDown()
+			
+			prevImg = curImg
+			dots.children().removeClass("selectedDot")
+			el.addClass("selectedDot")
+		})
+		dots.append(el)
+	}
+
+
+	
+	setInterval(()=>{
+		//indexPhoto - current image
+		$(`#caruselPhoto${indexPhoto}`).slideUp("fast")
+		
+		let next
+		if (indexPhoto + 1 >= images.length){
+			next = $(`#caruselPhoto${0}`)
+			indexPhoto = 0
+		} else
+			next = $(`#caruselPhoto${++indexPhoto}`)
+
+		next.slideDown()
+
+		dots.children().removeClass("selectedDot")
+		$(`.dots div:nth-child(${indexPhoto + 1})`).addClass("selectedDot")
+	}, 5000)
---
+++ script.js
@@ -0,0 +1,16 @@
+ScrollReveal().reveal("div")
+
+/*let header = document.querySelector("header")
+header.style.backgroundImage = "url(img/bg.jpg)"*/
+
+try {
+	let a = document.querySelector(".ph")
+	let b = a.getElementsByTagName("*")
+	for(el of b)
+		el.style.width = window.innerWidth / 4 - 5 + "px"
+
+	let all = document.querySelector("div")
+} catch(e) {
+	console.log(e)
+}
+

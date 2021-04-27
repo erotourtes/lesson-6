25de9d7342b0e97e0007b4bbdeb929734e1862a4
erotourtes <erotourtes@gmail.com>
2021-04-27 18:38:22 +0300

    download

---
+++ signin menu/sgin.js
@@ -0,0 +1,63 @@
+let btn = $("#toLogIn")
+let sh = $(".shadow")
+let panel = $("#logInPanel")
+
+sh.css({height: $(document).height() + "px"})
+panel.css({opacity: "1"})
+
+panel.hide()
+sh.hide()
+
+btn.on("click", ()=>{
+		
+	panel.fadeToggle()
+	sh.toggle()
+	$("body").css({overflow: "hidden"})
+})
+sh.on("click", ()=>{
+	panel.fadeToggle(100)
+	setTimeout(()=>sh.fadeOut(), 250)
+		
+	$("body").css({overflow: "visible"})
+})
+
+
+let ifEmail = false
+let ifPsss= false
+let e = $(".errMasages")
+
+$("#email").on("input", ()=>{
+	ifEmail = false
+	let val = $("#email").val()
+	let sub = val.substring(val.length-4, val.length)
+	valP = $("#password").val()
+
+	if (e.css("left") != "-500px")
+		e.animate({ opacity: "0",  left: "-500px",}, 500)
+
+	document.querySelector("#logbtn").setAttribute("onclick", "location.href='#'")
+
+	if(sub === ".com") 
+		ifEmail = true
+})
+$("#password").on("input", ()=>{
+	ifPsss = false
+	let valP = $("#password").val()
+	valP = $("#password").val()
+	if(valP.length > 5)
+		ifPsss = true
+
+	if (e.css("left") != "-500px")
+		e.animate({ opacity: "0",  left: "-500px",}, 500)
+})
+
+$("#logbtn").on("click", ()=>{
+	if(!ifEmail || !ifPsss){
+		e.animate({ opacity: "1",  left: "500px",}, 500)
+		$(".errMasages p").text("have no .com or password")
+		return
+	}
+
+	document.querySelector("#logbtn").setAttribute("onclick", "location.href='../index.html'")
+
+})
